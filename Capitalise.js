ddc572b0d2102d2b2b007eb2772507205beb9cc1
Aprajita Yadav <aprajita.yaduvanshi@gmail.com>
2020-04-08 10:50:06 -0400

    new file:   Capitalise.js

---
+++ Capitalise.js
@@ -0,0 +1,11 @@
+var cap={
+	myfunction : function () {
+  var array2 = ["a", "b","c","d","e"];
+   array2 = array2.map(function(myFunction){
+  	console.log (myFunction.toUpperCase())
+  	
+  })
+  	}
+  }
+
+cap.myfunction();
